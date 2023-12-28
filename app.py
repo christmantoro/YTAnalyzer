32a6e706f05d7c524085580ffe31283de24c3d2a
Anthony Christmantoro <christmantoro@gmail.com>
2023-12-28 13:55:33 +0700

    Small Change

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ youtube_url = "https://www.youtube.com/watch?v=UiYXwUg23Yw"
 
 yt=YouTube(youtube_url)
 
-if(yt):
-    print("Title: ", yt.title)
+try:
+    if(yt):
+        print("Title: ", yt.title)
+        print("Views: ", yt.views)
+        print("Author: ", yt.author)
+        print("Description: ", yt.description)
+except:
+    print("There is some problem .....")
